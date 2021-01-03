d014c00aaac6413d8373b7dee0dc4e82079d7cf7
Paul Schaub <vanitasvitae@fsfe.org>
2021-01-03 15:52:33 +0100

    Refactor implementationfactory

--- pgpainless-core/src/main/java/org/pgpainless/implementation/BcImplementationFactory.java
+++ pgpainless-core/src/main/java/org/pgpainless/implementation/BcImplementationFactory.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2020 Paul Schaub.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.pgpainless.implementation;
 
 import java.security.KeyPair;
@@ -40,7 +55,7 @@ import org.pgpainless.algorithm.PublicKeyAlgorithm;
 import org.pgpainless.algorithm.SymmetricKeyAlgorithm;
 import org.pgpainless.util.Passphrase;
 
-public class BcCryptoEngineImplementation implements CryptoEngineImplementation {
+public class BcImplementationFactory extends ImplementationFactory {
 
     @Override
     public PBESecretKeyEncryptor getPBESecretKeyEncryptor(PGPSecretKey secretKey, Passphrase passphrase)
--- pgpainless-core/src/main/java/org/pgpainless/implementation/JceImplementationFactory.java
+++ pgpainless-core/src/main/java/org/pgpainless/implementation/JceImplementationFactory.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2020 Paul Schaub.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.pgpainless.implementation;
 
 import java.io.IOException;
@@ -47,9 +62,8 @@ import org.pgpainless.algorithm.SymmetricKeyAlgorithm;
 import org.pgpainless.provider.ProviderFactory;
 import org.pgpainless.util.Passphrase;
 
-public class JceCryptoEngineImplementation implements CryptoEngineImplementation {
+public class JceImplementationFactory extends ImplementationFactory {
 
-    @Override
     public PBESecretKeyEncryptor getPBESecretKeyEncryptor(PGPSecretKey secretKey, Passphrase passphrase)
             throws PGPException {
         return new JcePBESecretKeyEncryptorBuilder(secretKey.getKeyEncryptionAlgorithm())
@@ -57,21 +71,18 @@ public class JceCryptoEngineImplementation implements CryptoEngineImplementation
                 .build(passphrase.getChars());
     }
 
-    @Override
     public PBESecretKeyEncryptor getPBESecretKeyEncryptor(SymmetricKeyAlgorithm symmetricKeyAlgorithm, PGPDigestCalculator digestCalculator, Passphrase passphrase) {
         return new JcePBESecretKeyEncryptorBuilder(symmetricKeyAlgorithm.getAlgorithmId(), digestCalculator)
                 .setProvider(ProviderFactory.getProvider())
                 .build(passphrase.getChars());
     }
 
-    @Override
     public PBESecretKeyDecryptor getPBESecretKeyDecryptor(Passphrase passphrase) throws PGPException {
         return new JcePBESecretKeyDecryptorBuilder(getPGPDigestCalculatorProvider())
                 .setProvider(ProviderFactory.getProvider())
                 .build(passphrase.getChars());
     }
 
-    @Override
     public PGPDigestCalculatorProvider getPGPDigestCalculatorProvider()
             throws PGPException {
         return new JcaPGPDigestCalculatorProviderBuilder()
@@ -79,25 +90,21 @@ public class JceCryptoEngineImplementation implements CryptoEngineImplementation
                 .build();
     }
 
-    @Override
     public PGPContentVerifierBuilderProvider getPGPContentVerifierBuilderProvider() {
         return new JcaPGPContentVerifierBuilderProvider()
                 .setProvider(ProviderFactory.getProvider());
     }
 
-    @Override
     public PGPContentSignerBuilder getPGPContentSignerBuilder(int keyAlgorithm, int hashAlgorithm) {
         return new JcaPGPContentSignerBuilder(keyAlgorithm, hashAlgorithm)
                 .setProvider(ProviderFactory.getProvider());
     }
 
-    @Override
     public KeyFingerPrintCalculator getKeyFingerprintCalculator() {
         return new JcaKeyFingerprintCalculator()
                 .setProvider(ProviderFactory.getProvider());
     }
 
-    @Override
     public PBEDataDecryptorFactory getPBEDataDecryptorFactory(Passphrase passphrase)
             throws PGPException {
         return new JcePBEDataDecryptorFactoryBuilder(getPGPDigestCalculatorProvider())
@@ -105,42 +112,35 @@ public class JceCryptoEngineImplementation implements CryptoEngineImplementation
                 .build(passphrase.getChars());
     }
 
-    @Override
     public PublicKeyDataDecryptorFactory getPublicKeyDataDecryptorFactory(PGPPrivateKey privateKey) {
         return new JcePublicKeyDataDecryptorFactoryBuilder()
                 .setProvider(ProviderFactory.getProvider())
                 .build(privateKey);
     }
 
-    @Override
     public PublicKeyKeyEncryptionMethodGenerator getPublicKeyKeyEncryptionMethodGenerator(PGPPublicKey key) {
         return new JcePublicKeyKeyEncryptionMethodGenerator(key)
                 .setProvider(ProviderFactory.getProvider());
     }
 
-    @Override
     public PBEKeyEncryptionMethodGenerator getPBEKeyEncryptionMethodGenerator(Passphrase passphrase) {
         return new JcePBEKeyEncryptionMethodGenerator(passphrase.getChars())
                 .setProvider(ProviderFactory.getProvider());
     }
 
-    @Override
     public PGPDataEncryptorBuilder getPGPDataEncryptorBuilder(int symmetricKeyAlgorithm) {
         return new JcePGPDataEncryptorBuilder(symmetricKeyAlgorithm)
                 .setProvider(ProviderFactory.getProvider());
     }
 
-    @Override
     public PGPKeyPair getPGPKeyPair(PublicKeyAlgorithm algorithm, KeyPair keyPair, Date creationDate) throws PGPException {
         return new JcaPGPKeyPair(algorithm.getAlgorithmId(), keyPair, creationDate);
     }
 
-    @Override
     public PGPKeyPair getPGPKeyPair(PublicKeyAlgorithm algorithm, AsymmetricCipherKeyPair keyPair, Date creationDate) throws PGPException, NoSuchAlgorithmException, IOException, InvalidKeySpecException {
         return new JcaPGPKeyPair(algorithm.getAlgorithmId(), bcToJceKeyPair(keyPair), creationDate);
     }
 
-    @Override
     public PBESecretKeyEncryptor getPBESecretKeyEncryptor(SymmetricKeyAlgorithm encryptionAlgorithm, HashAlgorithm hashAlgorithm, int s2kCount, Passphrase passphrase) throws PGPException {
         return new JcePBESecretKeyEncryptorBuilder(
                 encryptionAlgorithm.getAlgorithmId(),
