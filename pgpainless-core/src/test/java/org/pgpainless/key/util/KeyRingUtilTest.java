0acd6dea5c93ae7bda0977f9e9875fbd30b8f48f
Paul Schaub <vanitasvitae@fsfe.org>
2021-11-12 16:49:28 +0100

    Add tests for KeyRingUtils.deleteUserIdFrom*KeyRing methods

---
+++ pgpainless-core/src/test/java/org/pgpainless/key/util/KeyRingUtilTest.java
@@ -0,0 +1,53 @@
+// SPDX-FileCopyrightText: 2021 Paul Schaub <vanitasvitae@fsfe.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package org.pgpainless.key.util;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.security.InvalidAlgorithmParameterException;
+import java.security.NoSuchAlgorithmException;
+
+import org.bouncycastle.openpgp.PGPException;
+import org.bouncycastle.openpgp.PGPPublicKeyRing;
+import org.bouncycastle.openpgp.PGPSecretKeyRing;
+import org.junit.jupiter.api.Test;
+import org.pgpainless.PGPainless;
+import org.pgpainless.key.protection.SecretKeyRingProtector;
+import org.pgpainless.util.CollectionUtils;
+
+public class KeyRingUtilTest {
+
+    @Test
+    public void testDeleteUserIdFromSecretKeyRing()
+            throws PGPException, InvalidAlgorithmParameterException, NoSuchAlgorithmException {
+        PGPSecretKeyRing secretKeys = PGPainless.generateKeyRing()
+                .modernKeyRing("Alice", null);
+
+        secretKeys = PGPainless.modifyKeyRing(secretKeys)
+                .addUserId("Bob", SecretKeyRingProtector.unprotectedKeys())
+                .done();
+        assertEquals(2, CollectionUtils.iteratorToList(secretKeys.getPublicKey().getUserIDs()).size());
+
+        secretKeys = KeyRingUtils.deleteUserIdFromSecretKeyRing(secretKeys, "Bob");
+
+        assertEquals(1, CollectionUtils.iteratorToList(secretKeys.getPublicKey().getUserIDs()).size());
+    }
+
+    @Test
+    public void testDeleteUserIdFromPublicKeyRing() throws PGPException, InvalidAlgorithmParameterException, NoSuchAlgorithmException {
+        PGPSecretKeyRing secretKeys = PGPainless.generateKeyRing()
+                .modernKeyRing("Alice", null);
+
+        secretKeys = PGPainless.modifyKeyRing(secretKeys)
+                .addUserId("Bob", SecretKeyRingProtector.unprotectedKeys())
+                .done();
+        PGPPublicKeyRing publicKeys = PGPainless.extractCertificate(secretKeys);
+        assertEquals(2, CollectionUtils.iteratorToList(publicKeys.getPublicKey().getUserIDs()).size());
+
+        publicKeys = KeyRingUtils.deleteUserIdFromPublicKeyRing(publicKeys, "Alice");
+
+        assertEquals(1, CollectionUtils.iteratorToList(publicKeys.getPublicKey().getUserIDs()).size());
+    }
+}
