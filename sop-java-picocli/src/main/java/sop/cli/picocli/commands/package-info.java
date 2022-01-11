c6bc8f9774ed37e5575155f90922315b30ea8ead
Paul Schaub <vanitasvitae@fsfe.org>
2022-01-11 15:18:34 +0100

    Moved sop-java and sop-java-picocli to its own repositories
    See https://github.com/pgpainless/sop-java
    See https://codeberg.org/PGPainless/sop-java

--- sop-java-picocli/src/main/java/sop/cli/picocli/commands/package-info.java
+++
@@ -1,8 +0,0 @@
-// SPDX-FileCopyrightText: 2021 Paul Schaub <vanitasvitae@fsfe.org>
-//
-// SPDX-License-Identifier: Apache-2.0
-
-/**
- * Subcommands of the PGPainless SOP.
- */
-package sop.cli.picocli.commands;
--- sop-java/src/main/java/sop/operation/package-info.java
+++
@@ -1,9 +0,0 @@
-// SPDX-FileCopyrightText: 2021 Paul Schaub <vanitasvitae@fsfe.org>
-//
-// SPDX-License-Identifier: Apache-2.0
-
-/**
- * Stateless OpenPGP Interface for Java.
- * Different cryptographic operations.
- */
-package sop.operation;
