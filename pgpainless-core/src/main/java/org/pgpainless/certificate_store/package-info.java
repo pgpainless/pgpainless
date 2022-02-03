c0553654bc7479477f0cdbb04f49d2780f3f35cc
Paul Schaub <vanitasvitae@fsfe.org>
2022-02-03 15:43:21 +0100

    Move CertificateFactory + CertificateReader to pgpainless-cert-d

--- pgpainless-core/src/main/java/org/pgpainless/certificate_store/package-info.java
+++
@@ -1,8 +0,0 @@
-// SPDX-FileCopyrightText: 2022 Paul Schaub <vanitasvitae@fsfe.org>
-//
-// SPDX-License-Identifier: Apache-2.0
-
-/**
- * Implementations of the module pgp-certificate-store using pgpainless-core.
- */
-package org.pgpainless.certificate_store;
