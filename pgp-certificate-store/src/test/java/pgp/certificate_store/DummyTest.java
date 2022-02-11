0ef4f3b78149def3944239c5c9bcb60f767e57a6
Paul Schaub <vanitasvitae@fsfe.org>
2022-02-11 13:43:35 +0100

    Add dummy tests for coverage

---
+++ pgp-certificate-store/src/test/java/pgp/certificate_store/DummyTest.java
@@ -0,0 +1,16 @@
+// SPDX-FileCopyrightText: 2022 Paul Schaub <vanitasvitae@fsfe.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package pgp.certificate_store;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DummyTest {
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
---
+++ pgpainless-cert-d-cli/src/test/java/pgp/cert_d/cli/DummyTest.java
@@ -0,0 +1,17 @@
+// SPDX-FileCopyrightText: 2022 Paul Schaub <vanitasvitae@fsfe.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package pgp.cert_d.cli;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DummyTest {
+
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
