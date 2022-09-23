a3957d33724d0d30311082c5565ac970542563cb
Paul Schaub <vanitasvitae@fsfe.org>
2022-09-23 14:04:44 +0200

    WIP: Play around with TeeInputStreams

---
+++ pgpainless-core/src/main/java/org/pgpainless/decryption_verification/TeeBCPGInputStream.java
@@ -0,0 +1,35 @@
+package org.pgpainless.decryption_verification;
+
+import org.bouncycastle.bcpg.BCPGInputStream;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class TeeBCPGInputStream extends BCPGInputStream {
+
+    private final OutputStream out;
+
+    public TeeBCPGInputStream(InputStream in, OutputStream outputStream) {
+        super(in);
+        this.out = outputStream;
+    }
+
+    @Override
+    public int read() throws IOException {
+        int r = super.read();
+        if (r != -1) {
+            out.write(r);
+        }
+        return r;
+    }
+
+    @Override
+    public int read(byte[] buf, int off, int len) throws IOException {
+        int r = super.read(buf, off, len);
+        if (r > 0) {
+            out.write(buf, off, r);
+        }
+        return r;
+    }
+}
