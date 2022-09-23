1be4d8e5c427cfc247e5a2b5eb1f2c70927cf3ce
Paul Schaub <vanitasvitae@fsfe.org>
2022-09-23 14:51:06 +0200

    Implement caching PublicKeyDataDecryptorFactory

---
+++ pgpainless-core/src/main/java/org/bouncycastle/package-info.java
@@ -0,0 +1,8 @@
+// SPDX-FileCopyrightText: 2022 Paul Schaub <vanitasvitae@fsfe.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+/**
+ * Classes which could be upstreamed to BC at some point.
+ */
+package org.bouncycastle;
