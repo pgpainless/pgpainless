48d468c31070f5ba9c95b645d3fd8909ecb5fca4
Paul Schaub <vanitasvitae@fsfe.org>
2022-10-28 16:36:08 +0200

    Add and test GnuDummyKeyUtil

---
+++ pgpainless-core/src/main/java/org/pgpainless/key/gnu_dummy_s2k/package-info.java
@@ -0,0 +1,8 @@
+// SPDX-FileCopyrightText: 2022 Paul Schaub <vanitasvitae@fsfe.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+/**
+ * Utility classes related to creating keys with GNU DUMMY S2K values.
+ */
+package org.pgpainless.key.gnu_dummy_s2k;
