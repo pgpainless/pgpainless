a9d9b9baf677934cb8f9731e61b3d1ba6f8170fa
Paul Schaub <vanitasvitae@fsfe.org>
2022-11-16 17:38:38 +0100

    Enable previously disabled test for marker+seipd packet processing

--- pgpainless-core/src/test/java/org/pgpainless/signature/IgnoreMarkerPackets.java
+++ pgpainless-core/src/test/java/org/pgpainless/signature/IgnoreMarkerPackets.java
@@ -20,7 +20,6 @@ import org.bouncycastle.openpgp.PGPPublicKeyRing;
 import org.bouncycastle.openpgp.PGPSecretKeyRing;
 import org.bouncycastle.openpgp.PGPSignature;
 import org.bouncycastle.util.io.Streams;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.pgpainless.PGPainless;
 import org.pgpainless.decryption_verification.ConsumerOptions;
@@ -160,8 +159,6 @@ public class IgnoreMarkerPackets {
     }
 
     @Test
-    @Disabled
-    // TODO: Enable and fix
     public void markerPlusEncryptedMessage() throws IOException, PGPException {
         String msg = "-----BEGIN PGP MESSAGE-----\n" +
                 "\n" +
