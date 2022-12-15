ab6b6ca2e74fc192781d74db183ae47fab38e1ec
Paul Schaub <vanitasvitae@fsfe.org>
2022-12-15 12:30:30 +0100

    Add regression test for #351

--- pgpainless-sop/src/test/java/org/pgpainless/sop/GenerateKeyTest.java
+++ pgpainless-sop/src/test/java/org/pgpainless/sop/GenerateKeyTest.java
@@ -6,15 +6,20 @@ package org.pgpainless.sop;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
 
+import org.bouncycastle.openpgp.PGPException;
+import org.bouncycastle.openpgp.PGPSecretKey;
 import org.bouncycastle.openpgp.PGPSecretKeyRing;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.pgpainless.PGPainless;
 import org.pgpainless.key.info.KeyRingInfo;
+import org.pgpainless.key.protection.UnlockSecretKey;
+import org.pgpainless.util.Passphrase;
 import sop.SOP;
 
 public class GenerateKeyTest {
@@ -66,5 +71,25 @@ public class GenerateKeyTest {
                 .getBytes();
 
         assertFalse(new String(bytes).startsWith("-----BEGIN PGP PRIVATE KEY BLOCK-----"));
+    }
+
+    @Test
+    public void protectedMultiUserIdKey() throws IOException, PGPException {
+        byte[] bytes = sop.generateKey()
+                .userId("Alice")
+                .userId("Bob")
+                .withKeyPassword("sw0rdf1sh")
+                .generate()
+                .getBytes();
+
+        PGPSecretKeyRing secretKey = PGPainless.readKeyRing().secretKeyRing(bytes);
+        KeyRingInfo info = PGPainless.inspectKeyRing(secretKey);
+
+        assertTrue(info.getUserIds().contains("Alice"));
+        assertTrue(info.getUserIds().contains("Bob"));
+
+        for (PGPSecretKey key : secretKey) {
+            assertNotNull(UnlockSecretKey.unlockSecretKey(key, Passphrase.fromPassword("sw0rdf1sh")));
+        }
     }
 }
