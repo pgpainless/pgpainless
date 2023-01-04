980daeca31f5f1554f86f34eeb67164a7d73fc2b
Paul Schaub <vanitasvitae@fsfe.org>
2023-01-04 18:55:57 +0100

    Add missing javadoc to CustomPublicKeyDataDecryptorFactory

--- pgpainless-core/src/main/java/org/pgpainless/decryption_verification/CustomPublicKeyDataDecryptorFactory.java
+++ pgpainless-core/src/main/java/org/pgpainless/decryption_verification/CustomPublicKeyDataDecryptorFactory.java
@@ -7,8 +7,21 @@ package org.pgpainless.decryption_verification;
 import org.bouncycastle.openpgp.operator.PublicKeyDataDecryptorFactory;
 import org.pgpainless.key.SubkeyIdentifier;
 
+/**
+ * Custom {@link PublicKeyDataDecryptorFactory} which can enable customized implementations of message decryption
+ * using public keys.
+ * This class can for example be used to implement message encryption using hardware tokens like smartcards or
+ * TPMs.
+ * @see ConsumerOptions#addCustomDecryptorFactory(CustomPublicKeyDataDecryptorFactory)
+ */
 public interface CustomPublicKeyDataDecryptorFactory extends PublicKeyDataDecryptorFactory {
 
+    /**
+     * Return the {@link SubkeyIdentifier} for which this particular {@link CustomPublicKeyDataDecryptorFactory}
+     * is intended.
+     *
+     * @return subkey identifier
+     */
     SubkeyIdentifier getSubkeyIdentifier();
 
 }
