44608744c26f2df63daba7f3a8e24d706b8d5db0
Paul Schaub <vanitasvitae@fsfe.org>
2023-04-14 16:17:58 +0200

    Add missing license header

--- pgpainless-core/src/test/java/org/pgpainless/key/_64DigitFingerprintTest.java
+++ pgpainless-core/src/test/java/org/pgpainless/key/_64DigitFingerprintTest.java
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2023 Paul Schaub <vanitasvitae@fsfe.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
 package org.pgpainless.key;
 
 import org.bouncycastle.util.encoders.Hex;
