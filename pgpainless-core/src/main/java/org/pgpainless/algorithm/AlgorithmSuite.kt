1062f6a8bfb352222272da60ad6fee595b4c9144
Paul Schaub <vanitasvitae@fsfe.org>
2023-08-04 17:18:19 +0200

    Kotlin conversion: AlgorithmSuite

---
+++ pgpainless-core/src/main/java/org/pgpainless/algorithm/AlgorithmSuite.kt
@@ -0,0 +1,41 @@
+package org.pgpainless.algorithm
+
+class AlgorithmSuite(
+        symmetricKeyAlgorithms: List<SymmetricKeyAlgorithm>,
+        hashAlgorithms: List<HashAlgorithm>,
+        compressionAlgorithms: List<CompressionAlgorithm>) {
+
+    val symmetricKeyAlgorithms: Set<SymmetricKeyAlgorithm> = symmetricKeyAlgorithms.toSet()
+    val hashAlgorithms: Set<HashAlgorithm> = hashAlgorithms.toSet()
+    val compressionAlgorithms: Set<CompressionAlgorithm> = compressionAlgorithms.toSet()
+
+    companion object {
+
+        @JvmStatic
+        val defaultSymmetricKeyAlgorithms = listOf(
+                SymmetricKeyAlgorithm.AES_256,
+                SymmetricKeyAlgorithm.AES_192,
+                SymmetricKeyAlgorithm.AES_128)
+
+        @JvmStatic
+        val defaultHashAlgorithms = listOf(
+                HashAlgorithm.SHA512,
+                HashAlgorithm.SHA384,
+                HashAlgorithm.SHA256,
+                HashAlgorithm.SHA224)
+
+        @JvmStatic
+        val defaultCompressionAlgorithms = listOf(
+                CompressionAlgorithm.ZLIB,
+                CompressionAlgorithm.BZIP2,
+                CompressionAlgorithm.ZIP,
+                CompressionAlgorithm.UNCOMPRESSED)
+
+        @JvmStatic
+        val defaultAlgorithmSuite = AlgorithmSuite(
+                defaultSymmetricKeyAlgorithms,
+                defaultHashAlgorithms,
+                 defaultCompressionAlgorithms)
+    }
+
+}
