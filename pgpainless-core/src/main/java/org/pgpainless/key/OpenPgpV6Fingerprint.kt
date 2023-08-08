85bf15d8bd2bda173c13b59e0e409fca37419f8f
Paul Schaub <vanitasvitae@fsfe.org>
2023-08-08 15:45:55 +0200

    Kotlin conversion: OpenPgpFingerprints

---
+++ pgpainless-core/src/main/java/org/pgpainless/key/OpenPgpV6Fingerprint.kt
@@ -0,0 +1,25 @@
+// SPDX-FileCopyrightText: 2023 Paul Schaub <vanitasvitae@fsfe.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package org.pgpainless.key
+
+import org.bouncycastle.openpgp.PGPKeyRing
+import org.bouncycastle.openpgp.PGPPublicKey
+import org.bouncycastle.openpgp.PGPSecretKey
+
+/**
+ * This class represents a hex encoded, uppercase OpenPGP v6 fingerprint.
+ */
+class OpenPgpV6Fingerprint: _64DigitFingerprint {
+
+    constructor(fingerprint: String): super(fingerprint)
+    constructor(key: PGPPublicKey): super(key)
+    constructor(key: PGPSecretKey): super(key)
+    constructor(keys: PGPKeyRing): super(keys)
+    constructor(bytes: ByteArray): super(bytes)
+
+    override fun getVersion(): Int {
+        return 6
+    }
+}
