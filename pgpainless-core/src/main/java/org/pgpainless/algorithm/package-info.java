e57e74163c8c860493553b3d110b4261db383cb2
Paul Schaub <vanitasvitae@fsfe.org>
2023-08-23 13:15:00 +0200

    Finish Policy conversion and move kotlin classes to src/kotlin/

--- pgpainless-core/src/main/java/org/pgpainless/algorithm/package-info.java
+++
@@ -1,8 +0,0 @@
-// SPDX-FileCopyrightText: 2018 Paul Schaub <vanitasvitae@fsfe.org>
-//
-// SPDX-License-Identifier: Apache-2.0
-
-/**
- * Enums which map to OpenPGP's algorithm IDs.
- */
-package org.pgpainless.algorithm;
