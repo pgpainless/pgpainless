48af91efbf060a53ee32d26c5b0af7f884027448
Paul Schaub <vanitasvitae@fsfe.org>
2023-08-30 15:59:25 +0200

    Kotlin conversion: Cleartext Signature Framework

--- pgpainless-core/src/main/java/org/pgpainless/decryption_verification/cleartext_signatures/package-info.java
+++
@@ -1,8 +0,0 @@
-// SPDX-FileCopyrightText: 2021 Paul Schaub <vanitasvitae@fsfe.org>
-//
-// SPDX-License-Identifier: Apache-2.0
-
-/**
- * Classes related to cleartext signature verification.
- */
-package org.pgpainless.decryption_verification.cleartext_signatures;
---
+++ pgpainless-core/src/main/kotlin/org/pgpainless/decryption_verification/cleartext_signatures/InMemoryMultiPassStrategy.kt
@@ -0,0 +1,29 @@
+// SPDX-FileCopyrightText: 2023 Paul Schaub <vanitasvitae@fsfe.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package org.pgpainless.decryption_verification.cleartext_signatures
+
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+
+/**
+ * Implementation of the [MultiPassStrategy].
+ * This class keeps the read data in memory by caching the data inside a [ByteArrayOutputStream].
+ *
+ * Note, that this class is suitable and efficient for processing small amounts of data.
+ * For larger data like encrypted files, use of the [WriteToFileMultiPassStrategy] is recommended to
+ * prevent [OutOfMemoryError] and other issues.
+ */
+class InMemoryMultiPassStrategy : MultiPassStrategy {
+
+    private val cache = ByteArrayOutputStream()
+
+    override val messageOutputStream: ByteArrayOutputStream
+        get() = cache
+
+    override val messageInputStream: ByteArrayInputStream
+        get() = ByteArrayInputStream(getBytes())
+
+    fun getBytes(): ByteArray = messageOutputStream.toByteArray()
+}
