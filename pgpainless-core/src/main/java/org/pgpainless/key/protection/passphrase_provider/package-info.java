cd1a3e436ca80ce3750cc22d81ebf2bb15f724a0
Paul Schaub <vanitasvitae@fsfe.org>
2023-09-01 13:43:04 +0200

    Kotlin conversion: SecretKeyPassphraseProvider and subclasses
    This commit also adds a workaround to build.gradle which enables proper Java interop for
    Kotlin interfaces with default implementations

--- pgpainless-core/src/main/java/org/pgpainless/key/protection/passphrase_provider/package-info.java
+++
@@ -1,8 +0,0 @@
-// SPDX-FileCopyrightText: 2020 Paul Schaub <vanitasvitae@fsfe.org>
-//
-// SPDX-License-Identifier: Apache-2.0
-
-/**
- * Passphrase Provider classes.
- */
-package org.pgpainless.key.protection.passphrase_provider;
