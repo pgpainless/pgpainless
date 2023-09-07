5b89200ba9d580a51894cb6af013b294a0aa1f67
Paul Schaub <vanitasvitae@fsfe.org>
2023-09-07 14:29:17 +0200

    Kotlin conversion: CachingBcPublicKeyDataDecryptorFactory

--- pgpainless-core/src/main/java/org/bouncycastle/package-info.java
+++
@@ -1,8 +0,0 @@
-// SPDX-FileCopyrightText: 2022 Paul Schaub <vanitasvitae@fsfe.org>
-//
-// SPDX-License-Identifier: Apache-2.0
-
-/**
- * Classes which could be upstreamed to BC at some point.
- */
-package org.bouncycastle;
