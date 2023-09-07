89b73895f5ceb450a86cfa47a279127ba7806848
Paul Schaub <vanitasvitae@fsfe.org>
2023-09-07 15:03:06 +0200

    Kotlin conversion: ECDSA

--- pgpainless-core/src/main/java/org/pgpainless/key/generation/type/ecc/ecdsa/package-info.java
+++
@@ -1,8 +0,0 @@
-// SPDX-FileCopyrightText: 2020 Paul Schaub <vanitasvitae@fsfe.org>
-//
-// SPDX-License-Identifier: Apache-2.0
-
-/**
- * Classes related to ECDSA.
- */
-package org.pgpainless.key.generation.type.ecc.ecdsa;
