32e998305c85e31970a4a763fe2ea54ac44033cd
Paul Schaub <vanitasvitae@fsfe.org>
2023-09-07 15:03:06 +0200

    Kotlin conversion: ECDSA

---
+++ pgpainless-core/src/main/kotlin/org/pgpainless/key/generation/type/ecc/ecdsa/ECDSA.kt
@@ -0,0 +1,22 @@
+// SPDX-FileCopyrightText: 2023 Paul Schaub <vanitasvitae@fsfe.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package org.pgpainless.key.generation.type.ecc.ecdsa
+
+import org.bouncycastle.jce.spec.ECNamedCurveGenParameterSpec
+import org.pgpainless.algorithm.PublicKeyAlgorithm
+import org.pgpainless.key.generation.type.KeyType
+import org.pgpainless.key.generation.type.ecc.EllipticCurve
+
+class ECDSA private constructor(val curve: EllipticCurve) : KeyType {
+    override val name = "ECDSA"
+    override val algorithm = PublicKeyAlgorithm.ECDSA
+    override val bitStrength = curve.bitStrength
+    override val algorithmSpec = ECNamedCurveGenParameterSpec(curve.curveName)
+
+    companion object {
+        @JvmStatic
+        fun fromCurve(curve: EllipticCurve) = ECDSA(curve)
+    }
+}
