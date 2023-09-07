f1352862f17aff607aefdddb868767a1244974f9
Paul Schaub <vanitasvitae@fsfe.org>
2023-09-07 15:00:54 +0200

    Kotlin conversion: ECDH

--- pgpainless-core/src/main/java/org/pgpainless/key/generation/type/ecc/ecdh/package-info.java
+++
@@ -1,8 +0,0 @@
-// SPDX-FileCopyrightText: 2020 Paul Schaub <vanitasvitae@fsfe.org>
-//
-// SPDX-License-Identifier: Apache-2.0
-
-/**
- * Classes related to ECDH.
- */
-package org.pgpainless.key.generation.type.ecc.ecdh;
