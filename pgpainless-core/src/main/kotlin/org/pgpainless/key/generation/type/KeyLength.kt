1ebf8e1e6fa9c9f9664ef0f742b8a52d1b188253
Paul Schaub <vanitasvitae@fsfe.org>
2023-09-07 14:32:49 +0200

    Kotlin conversion: KeyLength

---
+++ pgpainless-core/src/main/kotlin/org/pgpainless/key/generation/type/KeyLength.kt
@@ -0,0 +1,10 @@
+// SPDX-FileCopyrightText: 2023 Paul Schaub <vanitasvitae@fsfe.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package org.pgpainless.key.generation.type
+
+interface KeyLength {
+    
+    fun getLength(): Int
+}
