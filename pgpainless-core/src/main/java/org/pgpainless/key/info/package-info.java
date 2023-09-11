004588576bb7fe9ce658dcff63a092a9cb1dea2d
Paul Schaub <vanitasvitae@fsfe.org>
2023-09-11 17:47:25 +0200

    Kotlin conversion: KeyRingInfo

--- pgpainless-core/src/main/java/org/pgpainless/key/info/package-info.java
+++
@@ -1,8 +0,0 @@
-// SPDX-FileCopyrightText: 2018 Paul Schaub <vanitasvitae@fsfe.org>
-//
-// SPDX-License-Identifier: Apache-2.0
-
-/**
- * Extract information from PGPKeyRings.
- */
-package org.pgpainless.key.info;
