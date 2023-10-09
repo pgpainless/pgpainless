41f56bdf99ab7db8d992f8bc9a447d95228f7a7a
Paul Schaub <vanitasvitae@fsfe.org>
2023-10-09 12:44:15 +0200

    Kotlin conversion: KeySpec

---
+++ pgpainless-core/src/main/kotlin/org/pgpainless/key/generation/KeySpec.kt
@@ -0,0 +1,28 @@
+// SPDX-FileCopyrightText: 2023 Paul Schaub <vanitasvitae@fsfe.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package org.pgpainless.key.generation
+
+import org.bouncycastle.openpgp.PGPSignatureSubpacketVector
+import org.pgpainless.algorithm.KeyFlag
+import org.pgpainless.key.generation.type.KeyType
+import org.pgpainless.signature.subpackets.SignatureSubpackets
+import org.pgpainless.signature.subpackets.SignatureSubpacketsHelper
+import java.util.*
+
+data class KeySpec(
+        val keyType: KeyType,
+        val subpacketGenerator: SignatureSubpackets,
+        val isInheritedSubPackets: Boolean,
+        val keyCreationDate: Date
+) {
+
+    val subpackets: PGPSignatureSubpacketVector
+        get() = SignatureSubpacketsHelper.toVector(subpacketGenerator)
+
+    companion object {
+        @JvmStatic
+        fun getBuilder(type: KeyType, vararg flags: KeyFlag) = KeySpecBuilder(type, *flags)
+    }
+}
