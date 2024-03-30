741d72eadcdc8b285e0c6237d286612635267e27
Paul Schaub <vanitasvitae@fsfe.org>
2024-03-30 19:20:12 +0100

    Document nature of tests in pgpainless-sop

---
+++ pgpainless-sop/src/test/java/org/pgpainless/sop/package-info.java
@@ -0,0 +1,9 @@
+// SPDX-FileCopyrightText: 2024 Paul Schaub <vanitasvitae@fsfe.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+/**
+ * Tests for the {@link sop.SOP} API, tailored to the behavior of PGPainless' implementation specifically.
+ * Generalized tests can be found in {@link sop.testsuite.pgpainless}.
+ */
+package org.pgpainless.sop;
---
+++ pgpainless-sop/src/test/java/sop/testsuite/pgpainless/package-info.java
@@ -0,0 +1,9 @@
+// SPDX-FileCopyrightText: 2024 Paul Schaub <vanitasvitae@fsfe.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+/**
+ * Generalized tests for the {@link sop.SOP} API.
+ * For tests tailored specifically to PGPainless' behavior, see {@link org.pgpainless.sop}.
+ */
+package sop.testsuite.pgpainless;
