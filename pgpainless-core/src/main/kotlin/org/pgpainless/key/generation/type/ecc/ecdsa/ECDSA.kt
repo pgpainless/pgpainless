1acda0e970792d9f35e0518be9097a456bb35d39
Paul Schaub <vanitasvitae@fsfe.org>
2025-01-28 21:20:56 +0100

    Adapt PGPKeyPairGenerator and remove support for generating ElGamal keys

--- pgpainless-core/src/main/kotlin/org/pgpainless/key/generation/type/ecc/ecdsa/ECDSA.kt
+++ pgpainless-core/src/main/kotlin/org/pgpainless/key/generation/type/ecc/ecdsa/ECDSA.kt
@@ -4,7 +4,10 @@
 
 package org.pgpainless.key.generation.type.ecc.ecdsa
 
+import org.bouncycastle.jcajce.provider.asymmetric.util.ECUtil
 import org.bouncycastle.jce.spec.ECNamedCurveGenParameterSpec
+import org.bouncycastle.openpgp.PGPKeyPair
+import org.bouncycastle.openpgp.operator.PGPKeyPairGenerator
 import org.pgpainless.algorithm.PublicKeyAlgorithm
 import org.pgpainless.key.generation.type.KeyType
 import org.pgpainless.key.generation.type.ecc.EllipticCurve
@@ -14,6 +17,10 @@ class ECDSA private constructor(val curve: EllipticCurve) : KeyType {
     override val algorithm = PublicKeyAlgorithm.ECDSA
     override val bitStrength = curve.bitStrength
     override val algorithmSpec = ECNamedCurveGenParameterSpec(curve.curveName)
+
+    override fun generateKeyPair(generator: PGPKeyPairGenerator): PGPKeyPair {
+        return ECUtil.getNamedCurveOid(curve.curveName).let { generator.generateECDSAKeyPair(it) }
+    }
 
     companion object {
         @JvmStatic fun fromCurve(curve: EllipticCurve) = ECDSA(curve)
