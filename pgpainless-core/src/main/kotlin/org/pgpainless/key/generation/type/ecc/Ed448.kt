dca9aa8cc24b092e33446220d25fb48124c0e9e7
Paul Schaub <vanitasvitae@fsfe.org>
2025-01-29 12:09:37 +0100

    Add new key types X25519, X448, Ed25519, Ed448

---
+++ pgpainless-core/src/main/kotlin/org/pgpainless/key/generation/type/ecc/Ed448.kt
@@ -0,0 +1,16 @@
+package org.pgpainless.key.generation.type.ecc
+
+import org.bouncycastle.openpgp.PGPKeyPair
+import org.bouncycastle.openpgp.operator.PGPKeyPairGenerator
+import org.pgpainless.algorithm.PublicKeyAlgorithm
+import org.pgpainless.key.generation.type.KeyType
+
+class Ed448 : KeyType {
+    override val name: String = "Ed448"
+    override val algorithm: PublicKeyAlgorithm = PublicKeyAlgorithm.ED448
+    override val bitStrength: Int = 456
+
+    override fun generateKeyPair(generator: PGPKeyPairGenerator): PGPKeyPair {
+        return generator.generateEd448KeyPair()
+    }
+}
