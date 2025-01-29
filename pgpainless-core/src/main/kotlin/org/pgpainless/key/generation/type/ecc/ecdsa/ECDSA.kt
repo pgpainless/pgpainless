443361ba03407f3b5b0f402865c607ac8188719d
Paul Schaub <vanitasvitae@fsfe.org>
2025-01-29 12:09:37 +0100

    Add new key types X25519, X448, Ed25519, Ed448

--- pgpainless-core/src/main/kotlin/org/pgpainless/key/generation/type/ecc/ecdsa/ECDSA.kt
+++ pgpainless-core/src/main/kotlin/org/pgpainless/key/generation/type/ecc/ecdsa/ECDSA.kt
@@ -5,7 +5,6 @@
 package org.pgpainless.key.generation.type.ecc.ecdsa
 
 import org.bouncycastle.jcajce.provider.asymmetric.util.ECUtil
-import org.bouncycastle.jce.spec.ECNamedCurveGenParameterSpec
 import org.bouncycastle.openpgp.PGPKeyPair
 import org.bouncycastle.openpgp.operator.PGPKeyPairGenerator
 import org.pgpainless.algorithm.PublicKeyAlgorithm
@@ -16,7 +15,6 @@ class ECDSA private constructor(val curve: EllipticCurve) : KeyType {
     override val name = "ECDSA"
     override val algorithm = PublicKeyAlgorithm.ECDSA
     override val bitStrength = curve.bitStrength
-    override val algorithmSpec = ECNamedCurveGenParameterSpec(curve.curveName)
 
     override fun generateKeyPair(generator: PGPKeyPairGenerator): PGPKeyPair {
         return ECUtil.getNamedCurveOid(curve.curveName).let { generator.generateECDSAKeyPair(it) }
