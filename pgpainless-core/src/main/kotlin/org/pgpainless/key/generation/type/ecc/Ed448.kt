2d6969510f8b6d6cf4ba7da33d3e6b3d5dfa6262
Paul Schaub <vanitasvitae@fsfe.org>
2025-01-29 12:14:50 +0100

    Add missing license headers

--- pgpainless-core/src/main/kotlin/org/pgpainless/key/generation/type/ecc/Ed448.kt
+++ pgpainless-core/src/main/kotlin/org/pgpainless/key/generation/type/ecc/Ed448.kt
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 Paul Schaub <vanitasvitae@fsfe.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
 package org.pgpainless.key.generation.type.ecc
 
 import org.bouncycastle.openpgp.PGPKeyPair
