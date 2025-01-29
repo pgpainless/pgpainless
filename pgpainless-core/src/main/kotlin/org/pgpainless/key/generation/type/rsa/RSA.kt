7ba9155f712a33ab46f01cafc351043a0d886d34
Paul Schaub <vanitasvitae@fsfe.org>
2025-01-29 12:09:37 +0100

    Add new key types X25519, X448, Ed25519, Ed448

--- pgpainless-core/src/main/kotlin/org/pgpainless/key/generation/type/rsa/RSA.kt
+++ pgpainless-core/src/main/kotlin/org/pgpainless/key/generation/type/rsa/RSA.kt
@@ -4,7 +4,6 @@
 
 package org.pgpainless.key.generation.type.rsa
 
-import java.security.spec.RSAKeyGenParameterSpec
 import org.bouncycastle.openpgp.PGPKeyPair
 import org.bouncycastle.openpgp.operator.PGPKeyPairGenerator
 import org.pgpainless.algorithm.PublicKeyAlgorithm
@@ -16,7 +15,6 @@ class RSA private constructor(length: RsaLength) : KeyType {
     override val name = "RSA"
     override val algorithm = PublicKeyAlgorithm.RSA_GENERAL
     override val bitStrength = length.length
-    override val algorithmSpec = RSAKeyGenParameterSpec(length.length, RSAKeyGenParameterSpec.F4)
 
     override fun generateKeyPair(generator: PGPKeyPairGenerator): PGPKeyPair {
         return generator.generateRsaKeyPair(bitStrength)
