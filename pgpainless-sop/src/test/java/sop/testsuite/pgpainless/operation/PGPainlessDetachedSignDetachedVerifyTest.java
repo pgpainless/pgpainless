9592b228b11966849f2ee019ad7899b61bcd2372
Paul Schaub <vanitasvitae@fsfe.org>
2025-02-05 12:06:25 +0100

    Disable ElGamal key tests

--- pgpainless-sop/src/test/java/sop/testsuite/pgpainless/operation/PGPainlessDetachedSignDetachedVerifyTest.java
+++ pgpainless-sop/src/test/java/sop/testsuite/pgpainless/operation/PGPainlessDetachedSignDetachedVerifyTest.java
@@ -8,11 +8,19 @@ import org.junit.jupiter.api.Disabled;
 import sop.SOP;
 import sop.testsuite.operation.DetachedSignDetachedVerifyTest;
 
+import java.io.IOException;
+
 public class PGPainlessDetachedSignDetachedVerifyTest extends DetachedSignDetachedVerifyTest {
 
     @Override
     @Disabled("Since we allow for dynamic cert loading, we can ignore this test")
     public void verifyMissingCertCausesMissingArg(SOP sop) {
         super.verifyMissingCertCausesMissingArg(sop);
+    }
+
+    @Override
+    @Disabled("Carol is an ElGamal key, which are no longer supported.")
+    public void signVerifyWithCarolKey(SOP sop) throws IOException {
+        super.signVerifyWithCarolKey(sop);
     }
 }
