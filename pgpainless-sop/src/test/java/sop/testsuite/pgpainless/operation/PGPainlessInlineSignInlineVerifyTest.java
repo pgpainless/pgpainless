e9d5a88b1f34156a255783a11a2cb4c2e53d8391
Paul Schaub <vanitasvitae@fsfe.org>
2025-02-05 12:06:25 +0100

    Disable ElGamal key tests

--- pgpainless-sop/src/test/java/sop/testsuite/pgpainless/operation/PGPainlessInlineSignInlineVerifyTest.java
+++ pgpainless-sop/src/test/java/sop/testsuite/pgpainless/operation/PGPainlessInlineSignInlineVerifyTest.java
@@ -4,8 +4,17 @@
 
 package sop.testsuite.pgpainless.operation;
 
+import org.junit.jupiter.api.Disabled;
+import sop.SOP;
 import sop.testsuite.operation.InlineSignInlineVerifyTest;
+
+import java.io.IOException;
 
 public class PGPainlessInlineSignInlineVerifyTest extends InlineSignInlineVerifyTest {
 
+    @Override
+    @Disabled("Carol is an ElGamal key, which is no longer supported.")
+    public void inlineSignVerifyCarol(SOP sop) throws IOException {
+        super.inlineSignVerifyCarol(sop);
+    }
 }
