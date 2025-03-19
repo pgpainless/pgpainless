04160fbe27c0d61db1eea707ebfb35612499ddc4
Paul Schaub <vanitasvitae@fsfe.org>
2025-03-19 10:59:05 +0100

    Fix javadoc parameter names

--- pgpainless-core/src/main/kotlin/org/pgpainless/policy/Policy.kt
+++ pgpainless-core/src/main/kotlin/org/pgpainless/policy/Policy.kt
@@ -50,7 +50,7 @@ class Policy(
      * regardless of usage date.
      *
      * @param defaultHashAlgorithm default hash algorithm
-     * @param algorithmTerminationDates map of acceptable algorithms and their termination dates
+     * @param acceptableHashAlgorithmsAndTerminationDates map of acceptable algorithms and their termination dates
      */
     class HashAlgorithmPolicy(
         val defaultHashAlgorithm: HashAlgorithm,
