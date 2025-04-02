2d678f8bb990f7ab98cbea69adc3b81e304fa5f5
Paul Schaub <vanitasvitae@fsfe.org>
2025-04-02 20:50:03 +0200

    Add OpenPGPSecretKey.unlock(Passphrase) extension method

--- pgpainless-core/src/main/kotlin/org/pgpainless/bouncycastle/extensions/OpenPGPKeyExtensions.kt
+++ pgpainless-core/src/main/kotlin/org/pgpainless/bouncycastle/extensions/OpenPGPKeyExtensions.kt
@@ -7,10 +7,15 @@ package org.pgpainless.bouncycastle.extensions
 import org.bouncycastle.openpgp.PGPPublicKeyEncryptedData
 import org.bouncycastle.openpgp.PGPSignature
 import org.bouncycastle.openpgp.api.OpenPGPKey
+import org.bouncycastle.openpgp.api.OpenPGPKey.OpenPGPPrivateKey
 import org.bouncycastle.openpgp.api.OpenPGPKey.OpenPGPSecretKey
+import org.pgpainless.util.Passphrase
 
 fun OpenPGPKey.getSecretKeyFor(pkesk: PGPPublicKeyEncryptedData): OpenPGPSecretKey? =
     this.getSecretKey(pkesk.keyIdentifier)
 
 fun OpenPGPKey.getSecretKeyFor(signature: PGPSignature): OpenPGPSecretKey? =
     this.getSecretKey(signature.fingerprint!!.keyIdentifier)
+
+fun OpenPGPKey.OpenPGPSecretKey.unlock(passphrase: Passphrase): OpenPGPPrivateKey =
+    this.unlock(passphrase.getChars())
