7a3673516277cee0a85aa36ce7f7d93e4e9bc7dd
Paul Schaub <vanitasvitae@fsfe.org>
2025-04-10 13:52:13 +0200

    Add PGPainlessCertifyValidateUserIdTest

---
+++ pgpainless-sop/src/test/java/sop/testsuite/pgpainless/operation/PGPainlessCertifyValidateUserIdTest.java
@@ -0,0 +1,11 @@
+// SPDX-FileCopyrightText: 2025 Paul Schaub <vanitasvitae@fsfe.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package sop.testsuite.pgpainless.operation;
+
+import sop.testsuite.operation.CertifyValidateUserIdTest;
+
+public class PGPainlessCertifyValidateUserIdTest extends CertifyValidateUserIdTest {
+
+}
