aaf88b8d3e918267e5955abbbab8fba178677b09
Paul Schaub <vanitasvitae@fsfe.org>
2025-05-08 14:56:35 +0200

    Remove usage of OpenPgpKeyAttributeUtil

--- pgpainless-core/src/main/java/org/pgpainless/key/package-info.java
+++
@@ -1,8 +0,0 @@
-// SPDX-FileCopyrightText: 2018 Paul Schaub <vanitasvitae@fsfe.org>
-//
-// SPDX-License-Identifier: Apache-2.0
-
-/**
- * Classes related to OpenPGP keys.
- */
-package org.pgpainless.key;
