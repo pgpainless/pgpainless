be5c2a01a123144189ab055cb7dc608b2b548d76
Paul Schaub <vanitasvitae@fsfe.org>
2025-05-08 16:54:39 +0200

    Port GnuPGDummyExtension implementation

--- pgpainless-core/src/main/java/org/gnupg/package-info.java
+++
@@ -1,8 +0,0 @@
-// SPDX-FileCopyrightText: 2022 Paul Schaub <vanitasvitae@fsfe.org>
-//
-// SPDX-License-Identifier: Apache-2.0
-
-/**
- * Utility classes related to creating keys with GNU DUMMY S2K values.
- */
-package org.gnupg;
