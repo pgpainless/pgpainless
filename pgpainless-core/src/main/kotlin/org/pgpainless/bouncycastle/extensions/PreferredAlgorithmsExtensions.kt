8c7e9e1b54c387d7bbb174cdafa78ed8178d1d23
Paul Schaub <vanitasvitae@fsfe.org>
2025-05-13 13:53:55 +0200

    Add documentation

--- pgpainless-core/src/main/kotlin/org/pgpainless/bouncycastle/extensions/PreferredAlgorithmsExtensions.kt
+++ pgpainless-core/src/main/kotlin/org/pgpainless/bouncycastle/extensions/PreferredAlgorithmsExtensions.kt
@@ -9,16 +9,19 @@ import org.pgpainless.algorithm.CompressionAlgorithm
 import org.pgpainless.algorithm.HashAlgorithm
 import org.pgpainless.algorithm.SymmetricKeyAlgorithm
 
+/** Convert the [PreferredAlgorithms] packet into a [Set] of [HashAlgorithm] preferences. */
 fun PreferredAlgorithms?.toHashAlgorithms(): Set<HashAlgorithm> {
     return this?.preferences?.asSequence()?.map { HashAlgorithm.requireFromId(it) }?.toSet()
         ?: setOf()
 }
 
+/** Convert the [PreferredAlgorithms] packet into a [Set] of [SymmetricKeyAlgorithm] preferences. */
 fun PreferredAlgorithms?.toSymmetricKeyAlgorithms(): Set<SymmetricKeyAlgorithm> {
     return this?.preferences?.asSequence()?.map { SymmetricKeyAlgorithm.requireFromId(it) }?.toSet()
         ?: setOf()
 }
 
+/** Convert the [PreferredAlgorithms] packet into a [Set] of [CompressionAlgorithm] preferences. */
 fun PreferredAlgorithms?.toCompressionAlgorithms(): Set<CompressionAlgorithm> {
     return this?.preferences?.asSequence()?.map { CompressionAlgorithm.requireFromId(it) }?.toSet()
         ?: setOf()
