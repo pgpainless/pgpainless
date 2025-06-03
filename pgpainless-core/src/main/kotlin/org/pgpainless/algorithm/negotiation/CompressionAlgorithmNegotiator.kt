d789d3e0c4945892c216bd8748f2615abcdfb916
Paul Schaub <vanitasvitae@fsfe.org>
2025-06-03 12:25:40 +0200

    Add test for CompressionAlgorithmNegotiator

--- pgpainless-core/src/main/kotlin/org/pgpainless/algorithm/negotiation/CompressionAlgorithmNegotiator.kt
+++ pgpainless-core/src/main/kotlin/org/pgpainless/algorithm/negotiation/CompressionAlgorithmNegotiator.kt
@@ -8,13 +8,31 @@ import org.pgpainless.algorithm.CompressionAlgorithm
 import org.pgpainless.policy.Policy
 
 fun interface CompressionAlgorithmNegotiator {
+
+    /**
+     * Negotiate a suitable [CompressionAlgorithm] by taking into consideration the [Policy], a
+     * user-provided [compressionAlgorithmOverride] and the users set of [orderedPreferences].
+     *
+     * @param policy implementations [Policy]
+     * @param compressionAlgorithmOverride user-provided [CompressionAlgorithm] override.
+     * @param orderedPreferences preferred compression algorithms taken from the users certificate
+     * @return negotiated [CompressionAlgorithm]
+     */
     fun negotiate(
         policy: Policy,
-        override: CompressionAlgorithm?,
+        compressionAlgorithmOverride: CompressionAlgorithm?,
         orderedPreferences: Set<CompressionAlgorithm>?
     ): CompressionAlgorithm
 
     companion object {
+
+        /**
+         * Static negotiation of compression algorithms. This implementation discards compression
+         * algorithm preferences and instead either returns the non-null algorithm override,
+         * otherwise the policies default hash algorithm.
+         *
+         * @return delegate implementation
+         */
         @JvmStatic
         fun staticNegotiation(): CompressionAlgorithmNegotiator =
             CompressionAlgorithmNegotiator { policy, override, _ ->
