d080a25a94af8f00fac33bacfc5795fff209bd9e
Paul Schaub <vanitasvitae@fsfe.org>
2026-01-08 19:43:44 +0100

    Add implementation of UpdateKeyTest

---
+++ pgpainless-sop/src/test/java/sop/testsuite/pgpainless/operation/PGPainlessUpdateKeyTest.java
@@ -0,0 +1,11 @@
+// SPDX-FileCopyrightText: 2026 Paul Schaub <vanitasvitae@fsfe.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package sop.testsuite.pgpainless.operation;
+
+import sop.testsuite.operation.UpdateKeyTest;
+
+public class PGPainlessUpdateKeyTest extends UpdateKeyTest {
+
+}
